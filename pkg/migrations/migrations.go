0e52554bb7881274082097ce7ebe09439de42776
Dima12334 <podolinnyjdima2@gmail.com>
2025-06-14 13:11:46 +0300

    refactor migration setup using solid principles

--- pkg/migrations/migrations.go
+++ pkg/migrations/migrations.go
@@ -10,39 +10,55 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-func ApplyMigrations(dsn, migrationsPath string, direction string) error {
+func ApplyMigrations(dsn, migrationsPath, direction string) (err error) {
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return fmt.Errorf("failed to connect to database: %w", err)
 	}
 	defer func() {
-		if closeErr := db.Close(); closeErr != nil {
+		closeErr := db.Close()
+		if closeErr != nil {
 			if err != nil {
-				err = fmt.Errorf("%w; failed to close db connection: %w", err, closeErr)
+				err = fmt.Errorf("%w; failed to close db connection: %v", err, closeErr)
 			} else {
 				err = fmt.Errorf("failed to close db connection: %w", closeErr)
 			}
 		}
 	}()
 
+	migrator, err := createMigrator(db, migrationsPath)
+	if err != nil {
+		return err
+	}
+
+	return applyDirection(migrator, direction)
+}
+
+func createMigrator(db *sql.DB, migrationsPath string) (*migrate.Migrate, error) {
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
-		return fmt.Errorf("failed to initialize migration driver: %w", err)
+		return nil, fmt.Errorf("failed to create migration driver: %w", err)
 	}
 
 	m, err := migrate.NewWithDatabaseInstance(migrationsPath, "postgres", driver)
 	if err != nil {
-		return fmt.Errorf("failed to initialize migrations: %w", err)
+		return nil, fmt.Errorf("failed to initialize migrations: %w", err)
 	}
 
+	return m, nil
+}
+
+func applyDirection(m *migrate.Migrate, direction string) error {
 	switch direction {
 	case "up":
-		if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-			return fmt.Errorf("failed to apply migrations up: %w", err)
+		err := m.Up()
+		if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+			return fmt.Errorf("migration up failed: %w", err)
 		}
 	case "down":
-		if err := m.Down(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-			return fmt.Errorf("failed to apply migrations down: %w", err)
+		err := m.Down()
+		if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+			return fmt.Errorf("migration down failed: %w", err)
 		}
 	default:
 		return fmt.Errorf("unknown migration direction: %s", direction)
