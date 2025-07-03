e9ecd02eb486d0027ddec3c8f4a82141c0204c58
Dima12334 <podolinnyjdima2@gmail.com>
2025-07-03 15:01:32 +0300

    fix linter

--- internal/imports_test.go
+++ internal/imports_test.go
@@ -7,25 +7,37 @@ import (
 )
 
 func Test_Service_ShouldNotDependOn_Handlers(t *testing.T) {
-	archtest.Package(t, "weather_forecast_sub/internal/service").ShouldNotDependOn("weather_forecast_sub/internal/handlers")
+	archtest.Package(t, "weather_forecast_sub/internal/service").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
 }
 
 func Test_Repository_ShouldNotDependOn_Service(t *testing.T) {
-	archtest.Package(t, "weather_forecast_sub/internal/repository").ShouldNotDependOn("weather_forecast_sub/internal/service")
+	archtest.Package(t, "weather_forecast_sub/internal/repository").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
 }
 
 func Test_Repository_ShouldNotDependOn_Handlers(t *testing.T) {
-	archtest.Package(t, "weather_forecast_sub/internal/repository").ShouldNotDependOn("weather_forecast_sub/internal/handlers")
+	archtest.Package(t, "weather_forecast_sub/internal/repository").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
 }
 
 func Test_Domain_ShouldNotDependOn_Handlers(t *testing.T) {
-	archtest.Package(t, "weather_forecast_sub/internal/domain").ShouldNotDependOn("weather_forecast_sub/internal/handlers")
+	archtest.Package(t, "weather_forecast_sub/internal/domain").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
 }
 
 func Test_Domain_ShouldNotDependOn_Service(t *testing.T) {
-	archtest.Package(t, "weather_forecast_sub/internal/domain").ShouldNotDependOn("weather_forecast_sub/internal/service")
+	archtest.Package(t, "weather_forecast_sub/internal/domain").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
 }
 
 func Test_Domain_ShouldNotDependOn_Repository(t *testing.T) {
-	archtest.Package(t, "weather_forecast_sub/internal/domain").ShouldNotDependOn("weather_forecast_sub/internal/repository")
+	archtest.Package(t, "weather_forecast_sub/internal/domain").ShouldNotDependOn(
+		"weather_forecast_sub/internal/repository",
+	)
 }
