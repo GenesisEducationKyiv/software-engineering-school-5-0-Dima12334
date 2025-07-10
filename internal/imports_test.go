8a9e6e19c066ba602b8733c76c3a29cb7a411d2d
Dima12334 <podolinnyjdima2@gmail.com>
2025-07-10 12:25:24 +0300

    add imports tests

--- internal/imports_test.go
+++ internal/imports_test.go
@@ -7,37 +7,211 @@ import (
 )
 
 func Test_Service_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
 	archtest.Package(t, "weather_forecast_sub/internal/service").ShouldNotDependOn(
 		"weather_forecast_sub/internal/handlers",
 	)
 }
 
 func Test_Repository_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
 	archtest.Package(t, "weather_forecast_sub/internal/repository").ShouldNotDependOn(
 		"weather_forecast_sub/internal/service",
 	)
 }
 
 func Test_Repository_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
 	archtest.Package(t, "weather_forecast_sub/internal/repository").ShouldNotDependOn(
 		"weather_forecast_sub/internal/handlers",
 	)
 }
 
 func Test_Domain_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
 	archtest.Package(t, "weather_forecast_sub/internal/domain").ShouldNotDependOn(
 		"weather_forecast_sub/internal/handlers",
 	)
 }
 
 func Test_Domain_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
 	archtest.Package(t, "weather_forecast_sub/internal/domain").ShouldNotDependOn(
 		"weather_forecast_sub/internal/service",
 	)
 }
 
 func Test_Domain_ShouldNotDependOn_Repository(t *testing.T) {
+	t.Parallel()
 	archtest.Package(t, "weather_forecast_sub/internal/domain").ShouldNotDependOn(
+		"weather_forecast_sub/internal/repository",
+	)
+}
+
+func Test_PkgCache_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/cache").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
+}
+
+func Test_PkgCache_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/cache").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
+}
+
+func Test_PkgCache_ShouldNotDependOn_Repository(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/cache").ShouldNotDependOn(
+		"weather_forecast_sub/internal/repository",
+	)
+}
+
+func Test_PkgClients_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/clients").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
+}
+
+func Test_PkgClients_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/clients").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
+}
+
+func Test_PkgClients_ShouldNotDependOn_Repository(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/clients").ShouldNotDependOn(
+		"weather_forecast_sub/internal/repository",
+	)
+}
+
+func Test_PkgEmail_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/email").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
+}
+
+func Test_PkgEmail_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/email").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
+}
+
+func Test_PkgEmail_ShouldNotDependOn_Repository(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/email").ShouldNotDependOn(
+		"weather_forecast_sub/internal/repository",
+	)
+}
+
+func Test_PkgSMPT_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/email/smtp").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
+}
+
+func Test_PkgSMPT_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/email/smtp").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
+}
+
+func Test_PkgSMPT_ShouldNotDependOn_Repository(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/email/smtp").ShouldNotDependOn(
+		"weather_forecast_sub/internal/repository",
+	)
+}
+
+func Test_PkgErrors_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/errors").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
+}
+
+func Test_PkgErrors_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/errors").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
+}
+
+func Test_PkgErrors_ShouldNotDependOn_Repository(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/errors").ShouldNotDependOn(
+		"weather_forecast_sub/internal/repository",
+	)
+}
+
+func Test_PkgHash_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/hash").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
+}
+
+func Test_PkgHash_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/hash").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
+}
+
+func Test_PkgHash_ShouldNotDependOn_Repository(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/hash").ShouldNotDependOn(
+		"weather_forecast_sub/internal/repository",
+	)
+}
+
+func Test_PkgLogger_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/logger").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
+}
+
+func Test_PkgLogger_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/logger").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
+}
+
+func Test_PkgLogger_ShouldNotDependOn_Repository(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/logger").ShouldNotDependOn(
+		"weather_forecast_sub/internal/repository",
+	)
+}
+
+func Test_PkgMigrations_ShouldNotDependOn_Handlers(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/migrations").ShouldNotDependOn(
+		"weather_forecast_sub/internal/handlers",
+	)
+}
+
+func Test_PkgMigrations_ShouldNotDependOn_Service(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/migrations").ShouldNotDependOn(
+		"weather_forecast_sub/internal/service",
+	)
+}
+
+func Test_PkgMigrations_ShouldNotDependOn_Repository(t *testing.T) {
+	t.Parallel()
+	archtest.Package(t, "weather_forecast_sub/pkg/migrations").ShouldNotDependOn(
 		"weather_forecast_sub/internal/repository",
 	)
 }
